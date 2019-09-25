6f7f4253b52ff2239df7c2e25e6164180a318166
SQLPowerUser <38347459+SQLPowerUser@users.noreply.github.com>
2019-09-25 12:02:14 +0300

    Add files via upload

--- Todo List (simple)/app.js
+++ Todo List (simple)/app.js
@@ -1,11 +1,35 @@
 +function () {
 	'use strict';
 
+	function gid(i) {return document.getElementById(i);}
+	function QS(el,s) {return el.querySelector(s);}
+	function CEL(s) {return document.createElement(s);}
+	function ACH(p,c) {p.appendChild(c);}
+
+	function showModal(el) {
+		if (!gid('modal-over')) {
+			let fogDiv = CEL('div');
+			fogDiv.id = 'modal-over';
+			fogDiv.onmousedown = function() {return false};
+			ACH(document.body, fogDiv);
+		}
+		el.style.display = 'block';
+	}
+
+	function hideModal(el) {
+		try {
+			el.style.display = 'none';
+			gid('modal-over').remove();
+		} catch (e) {};
+	}
+
+
+
 	const initData = `[
 			{"text": "Task 99"},{"text": "Task 98"},{"text": "Task 97"},{"text": "Task 96"},{"text": "Task 95"},{"text": "Task 94"},{"text": "Task 93"},{"text": "Task 92"},{"text": "Task 91"},{"text": "Task 90"},{"text": "Task 89"},{"text": "Task 88"},{"text": "Task 87"},{"text": "Task 86"},{"text": "Task 85"},{"text": "Task 84"},{"text": "Task 83"},{"text": "Task 82"},{"text": "Task 81"},{"text": "Task 80"},{"text": "Task 79"},{"text": "Task 78"},{"text": "Task 77"},{"text": "Task 76"},{"text": "Task 75"},{"text": "Task 74"},{"text": "Task 73"},{"text": "Task 72"},{"text": "Task 71"},{"text": "Task 70"},
 			{"text": "Task 69"},{"text": "Task 68"},{"text": "Task 67"},{"text": "Task 66"},{"text": "Task 65"},{"text": "Task 64"},{"text": "Task 63"},{"text": "Task 62"},{"text": "Task 61"},{"text": "Task 60"},{"text": "Task 59"},{"text": "Task 58"},{"text": "Task 57"},{"text": "Task 56"},{"text": "Task 55"},{"text": "Task 54"},{"text": "Task 53"},{"text": "Task 52"},{"text": "Task 51"},{"text": "Task 50"},{"text": "Task 49"},{"text": "Task 48"},{"text": "Task 47"},{"text": "Task 46"},{"text": "Task 45"},{"text": "Task 44"},{"text": "Task 43"},{"text": "Task 42"},{"text": "Task 41"},{"text": "Task 40"},
 			{"text": "Task 39"},{"text": "Task 38"},{"text": "Task 37"},{"text": "Task 36"},{"text": "Task 35"},{"text": "Task 34"},{"text": "Task 33"},{"text": "Task 32"},{"text": "Task 31"},{"text": "Task 30"},{"text": "Task 29"},{"text": "Task 28"},{"text": "Task 27"},{"text": "Task 26"},{"text": "Task 25"},{"text": "Task 24"},{"text": "Task 23"},{"text": "Task 22"},{"text": "Task 21"},{"text": "Task 20"},{"text": "Task 19"},{"text": "Task 18"},{"text": "Task 17"},{"text": "Task 16"},{"text": "Task 15"},{"text": "Task 14"},{"text": "Task 13"},{"text": "Task 12"},
-			{"text": "Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться."},
+			{"text": "Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться. Длинный текст, общий список задач должен прокручиваться."},
 			{"text": "Task 10"},{"text": "Task 9"},{"text": "Task 8"},{"text": "Task 7"},{"text": "Task 6"},{"text": "Task 5"},{"text": "Task 4"},{"text": "Task 3"},{"text": "Task 2"}, {"text": "Task 1"},
 			{"text": "Несложная задача"},
 			{"text": "Лёгкая задача"},
@@ -23,7 +47,7 @@
 		getters: {
 			notesReverse(state) {
 				return state.notes.map( (el, i, arr) =>
-					({idx: arr.length - i, text: el.text})
+					({id: arr.length - i, text: el.text})
 				).reverse();
 			},
 			pageCount(state) {
@@ -32,13 +56,14 @@
 		}, // getters
 
 		mutations: {
-			loadInit(state, note) {
+			loadInit(state) {
 				state.notes = JSON.parse(initData);
 			},
 
 			changePage(state, page) {
 				state.pageNumber = page-1;
 			},
+
 			nextPage(state) {
 				state.pageNumber++;
 			},
@@ -50,8 +75,14 @@
 			addNote(state, note) {
 				state.notes.push({text: note});
 			},
-			deleteNote(state, idx) {
-				state.notes.splice(-idx, 1);
+
+			updateNote(state, p) {
+				let id = state.notes.length - p.id;
+				state.notes[id].text = p.text;
+			},
+
+			deleteNote(state, id) {
+				state.notes.splice(-id, 1);
 			},
 		}, // mutations
 
@@ -71,7 +102,9 @@
 		template: '#todo-template',
 		data() {
 			return {
-				newNote: '',
+				newText: '',
+				currentText: '',
+				docId: '',
 				showEdit: false
 			}
 		},
@@ -81,21 +114,39 @@
 				let start = store.state.pageNumber * store.state.size,
 				    end = start + store.state.size;
 				return store.getters.notesReverse.slice(start, end);
-			},
-			trimNote() {
-				return this.newNote.trim();
 			}
 		}, // computed
 
 		methods: {
-			addCard() {
-				if (!this.trimNote) {return;}
-				store.dispatch('addNote', this.trimNote);
+			addNote() {
+				let txt = this.newText.trim();
+				if (!txt) {return;}
+				store.dispatch('addNote', txt);
 				store.commit('changePage', 1);
-				this.newNote = '';
+				this.newText = '';
 			},
-			deleteNote(idx) {
-				store.commit('deleteNote', idx);
+
+			editNote(docId, docText) {
+				showModal(gid('edit-window'));
+				QS(gid('edit-window'), 'textarea').focus();
+				this.docId = docId;
+				this.currentText = docText;
+			},
+
+			updateNote() {
+				let txt = this.currentText.trim();
+				if (!txt) {return;}
+				let params = {id: this.docId, text: txt};
+				store.commit('updateNote', params);
+				hideModal(gid('edit-window'));
+			},
+
+			cancelNote() {
+				hideModal(gid('edit-window'));
+			},
+
+			deleteNote(id) {
+				store.commit('deleteNote', id);
 			},
 		},
 	}); // Vue.component('todo')
@@ -148,8 +199,14 @@
 			hasFirst: function() {
 				return this.rangeStart !== 1
 			},
+			hasDiffFirst: function() {
+				return this.current - this.pageRange - 2 > 0
+			},
 			hasLast: function() {
 				return this.rangeEnd < this.pageCount
+			},
+			hasDiffLast: function() {
+				return this.current + this.pageRange + 2 <= this.pageCount
 			},
 			hasPrev: function() {
 				return this.current > 1
